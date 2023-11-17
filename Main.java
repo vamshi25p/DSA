d9f56268bb456244651f76d74a8accf96dcbca8a
Vamshi Krishna <vamshikrishna788sai@gmail.com>
2023-11-17 18:20:34 +0530

    Creating a Array class.

---
+++ Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String args[]) {
+        Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
+        numbers.print();
+    }
+}
