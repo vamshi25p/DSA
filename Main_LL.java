8033106be3456c669ad7715cd8a38f1c9dad93fa
Vamshi Krishna <vamshikrishna788sai@gmail.com>
2023-11-22 18:21:59 +0530

    Linked List(inserion, deletion at first and Last).

---
+++ Main_LL.java
@@ -0,0 +1,11 @@
+public class Main_LL {
+    public static void main(String args[]) {
+        var list = new LinkedList();
+        list.addFirst(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.removeLast();
+    }
+}
